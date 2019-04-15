5c0585179b9eb839db2689d244fa70dc369e1ea1
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-04-15 14:40:08 +0300

    add javadoc for ArrayOperationsTest.java

--- unittesting/src/test/java/operations/ArrayOperationsTest.java
+++ unittesting/src/test/java/operations/ArrayOperationsTest.java
@@ -6,40 +6,74 @@ import org.junit.Test;
 import filehandlers.FileIO;
 import static org.mockito.Mockito.*;
 
+/**
+ * This class contains tests for the methods of ArrayOperaions class 
+ * @author Marietta Lazana
+ */
 public class ArrayOperationsTest {
 
 	String src = "src/test/resources/validFileInput.txt";
 
+	/**
+	 * Finds max number within a given file by mocking FileIO and InegerOperation
+	 * class
+	 * 
+	 * @param file the array with the given input
+	 * @return max integer of the array
+	 */
 	@Test
 	public void test_findMax() {
 
+		// Mock the FileIO dependency
 		FileIO fileio = mock(FileIO.class);
+		// Mock the IntegerOperations dependency
 		IntegerOperations io = mock(IntegerOperations.class);
 
 		ArrayOperations ao = new ArrayOperations(fileio, io);
 
-		when(fileio.readFile(src)).thenReturn(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+		int[] file = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		when(fileio.readFile(src)).thenReturn(file);
 		Assert.assertEquals(10, ao.findMaxInFile(src));
 
 	}
 
+	/**
+	 * Reads empty file as an ipnut
+	 * 
+	 * @param emptyFile array without integers
+	 * @exception IllegalArgumentException when file is empty
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void test_findMaxEmpty() {
 
+		// Mock the FileIO dependency
 		FileIO fileio = mock(FileIO.class);
+		// Mock the IntegerOperations dependency
 		IntegerOperations io = mock(IntegerOperations.class);
 
 		ArrayOperations ao = new ArrayOperations(fileio, io);
-		when(fileio.readFile(src)).thenReturn(new int[] {});
+
+		int[] emptyFile = {};
+		when(fileio.readFile(src)).thenReturn(emptyFile);
 
 		ao.findMaxInFile(src);
 	}
 
+	/**
+	 * Reverse a given array
+	 * 
+	 * @param fileInput an array with integers as input
+	 * @result the reversed array
+	 */
 	@Test
 	public void test_reverseArray_Mocking() {
 
+		// Mock the FileIO dependency
 		FileIO fileio = mock(FileIO.class);
+		// Mock the IntegerOperations dependency
 		IntegerOperations io = mock(IntegerOperations.class);
+
 		ArrayOperations ao = new ArrayOperations(fileio, io);
 
 		int[] fileInput = { 1, 2, 3, 4, 5 };
@@ -54,16 +88,23 @@ public class ArrayOperationsTest {
 		ao.reverseArray(src);
 
 	}
-	
+
+	/**
+	 * Empty array as an input 
+	 * @exception IllegalArgumentException when the given array is empty
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void test_reverseArrayEmpty() {
-		
+
+		// Mock the FileIO dependency
 		FileIO fileio = mock(FileIO.class);
+		// Mock the IntegerOperations dependency
 		IntegerOperations io = mock(IntegerOperations.class);
-		
+
 		ArrayOperations ao = new ArrayOperations(fileio, io);
-		when(fileio.readFile(src)).thenReturn(new int[] {});
 		
+		when(fileio.readFile(src)).thenReturn(new int[] {});
+
 		ao.reverseArray(src);
 	}
 
