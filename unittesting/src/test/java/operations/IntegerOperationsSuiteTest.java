3e3e5907e4c222344a44d841dbc6ca7989679dbd
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-04-22 14:29:46 +0300

    change class name of unit tests

---
+++ unittesting/src/test/java/operations/IntegerOperationsSuiteTest.java
@@ -0,0 +1,16 @@
+package operations;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test Suite for the classes IntegerOperationsTest_PowerOfTwo_Parameterized
+ * and IntegerOperationsTest
+ * @author Marietta Lazana
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({IntegerOperations_PowerOfTwo_ParameterizedTest.class,IntegerOperationsTest.class})
+public class IntegerOperationsSuiteTest {
+
+}
---
+++ unittesting/src/test/java/operations/IntegerOperations_PowerOfTwo_ParameterizedTest.java
@@ -0,0 +1,44 @@
+package operations;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Test class for IntegerOpertions class tha runs for 
+ * multiple input values
+ * @author Marietta Lazana
+ *
+ */
+@RunWith(Parameterized.class)
+public class IntegerOperations_PowerOfTwo_ParameterizedTest {
+	
+	@Parameter (value = 0)
+	public int power;
+	
+	@Parameter (value = 1)
+	public int result;
+	
+	IntegerOperations inop = new IntegerOperations();
+	
+	@Parameters
+	public static Collection<Object[]> data(){
+		Object[][] data = new Object[][]{{2,4},{3,8},{4,16}};
+		
+		return Arrays.asList(data);
+	}
+	/**
+	 * @return the power of two of a number
+	 */
+	@Test
+	public void test_powerOfTwo() {
+		Assert.assertEquals(result, inop.powerOfTwo(power));
+	}
+	
+}
